05c6d188d1a9725b07945dd55984a878d0f88a06
Aisyah Mahadma <ais.msbp@gmail.com>
2026-09-09 23:03:57 +0700

    [P-2] setup routing resource semua tabel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoanController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resource('books', BookController::class);
+Route::resource('categories', CategoryController::class)->except(['show']);
+Route::resource('loans', LoanController::class);
+Route::resource('members', MemberController::class);
+Route::put('loans/{loan}/return', [LoanController::class, 'returnBook'])->name('loans.return');
+Route::prefix('admin')->group(function () {
+    Route::get('/info', function () {
+        return 'Admin Info Page';
+    });
 });
